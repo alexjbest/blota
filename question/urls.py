6d53bf18292eaa754e8bfe822aec93b8e688c7cd
Alex J. Best <alex.j.best@gmail.com>
2015-03-02 22:14:41 +0000

    allow specification of a single file/module for testing

--- question/urls.py
+++ question/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import patterns, url
 from question import views
 
 urlpatterns = patterns('',
-        url(r'^$', views.index, name='index')
+        url(r'^$', views.index, name='index'),
+        url(r'^(?P<name>\w+)/$', views.file, name='file'),
         )
--- question/views.py
+++ question/views.py
@@ -7,3 +7,10 @@ def index(request):
 
     context = { 'rand_q': rand_q }
     return render(request, 'question/index.html', context)
+
+def file(request, name):
+    rand_q = Question.objects.filter(file__name = name).order_by('?')[0]
+    output = rand_q.question
+
+    context = { 'rand_q': rand_q }
+    return render(request, 'question/index.html', context)
