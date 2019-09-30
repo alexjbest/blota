db8084336ca5ddb717e7587eac2a3852fb7f8fe0
Alex J. Best <alex.j.best@gmail.com>
2019-09-30 07:22:02 +0000

    robots begone

--- blota/urls.py
+++ blota/urls.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.conf.urls import patterns, include, url
 from django.conf.urls.static import static
+from django.http import HttpResponse
 
 from django.contrib import admin
 admin.autodiscover()
@@ -9,6 +10,8 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'blota.views.home', name='home'),
     url(r'^admin/', include(admin.site.urls)),
+    (r'^robots.txt$', lambda r: HttpResponse("User-agent: *\nDisallow: /", mimetype="text/plain")),
     url(r'^', include('question.urls')),
 
 ) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
